19f0269afee64b3c540274fc17c9b95c389f8f1d
Yonatan Rojas S <yonatan@bedu.org>
2018-06-04 11:04:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.prueba').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2500
+  })
+})
